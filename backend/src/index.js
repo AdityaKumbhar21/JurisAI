b309cd1295ce4a4fc025d76aca564c35c0a3646d
Aditya Kumbhar <adityakumbhar915@gmail.com>
2025-03-25 22:19:22 +0530

    Production setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser"
 import { dbConnection } from "./config/db.config.js";
 import cors from "cors";
 dotenv.config();
+import path from "path";
 
 // applying middelwares
 app.use(express.json());
@@ -16,11 +17,21 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }))
-
+ const __dirname = path.resolve();
 
 // adding routes
 app.use("/api/auth", authRoutes);
 app.use("/api/doc", documentRoutes);
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 const port = process.env.PORT;
 app.listen(port, ()=>{
